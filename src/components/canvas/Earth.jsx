d0f3a2be17d96edc686dc4c7cd3ff0f75e7e1883
Akif Ahmed <akifahmed90@gmail.com>
2023-06-26 13:23:49 +0530

    added star canvas but it does not work

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,13 +1,12 @@
-import {Suspense} from "react";
+import React, {Suspense} from "react";
 import {Canvas} from "@react-three/fiber";
 import {OrbitControls, Preload, useGLTF} from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
-import {Camera} from "three";
 
 const Earth = () => {
-  //hook to import 3d model
   const earth = useGLTF("./planet/scene.gltf");
+
   return (
     <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
   );
@@ -18,8 +17,14 @@ const EarthCanvas = () => {
     <Canvas
       shadows
       frameloop="demand"
+      dpr={[1, 2]}
       gl={{preserveDrawingBuffer: true}}
-      camera={{fov: 45, near: 0.1, far: 200, position: [-4, 3, 6]}}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
@@ -29,8 +34,11 @@ const EarthCanvas = () => {
           minPolarAngle={Math.PI / 2}
         />
         <Earth />
+
+        <Preload all />
       </Suspense>
     </Canvas>
   );
 };
+
 export default EarthCanvas;
